50f8b0b758c89d63e2a606b4e8dd9a466b101b3a
msashank910 <mannavas@ufl.edu>
2022-12-04 23:24:18 -0500

    Splaytreeeeeee

---
+++ SplayTree.h
@@ -0,0 +1,16 @@
+
+
+
+#ifndef POLICE_SHOOTING_TREE_COMPARISONN_SPLAYTREE_H
+#define POLICE_SHOOTING_TREE_COMPARISONN_SPLAYTREE_H
+
+
+class SplayTree {
+    struct Node {
+        Node* left;
+        Node* right;
+    };
+};
+
+
+#endif //POLICE_SHOOTING_TREE_COMPARISONN_SPLAYTREE_H
