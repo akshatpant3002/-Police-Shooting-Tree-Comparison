8d75529569e90cb8e40de770929274ad47056880
Aryaan Verma <verma.aryaan@gmail.com>
2022-12-05 22:35:04 -0500

    added onto main

---
+++ cmake-build-debug/CMakeFiles/Police_Shooting_Tree_Comparisonn.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Police_Shooting_Tree_Comparisonn.dir/main.cpp.o"
+  "Police_Shooting_Tree_Comparisonn"
+  "Police_Shooting_Tree_Comparisonn.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Police_Shooting_Tree_Comparisonn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
