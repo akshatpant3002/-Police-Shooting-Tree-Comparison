1cd926a13b4c6353653572e649bcf319a27dcb64
akshatpant3002 <akshatpant3002@gmail.com>
2022-12-06 00:19:58 -0500

    test

--- maxBinaryHeap.h
+++ maxBinaryHeap.h
@@ -7,15 +7,16 @@
 #include "input.h"
 
 class maxBinaryHeap {
-    input *node;
     int maxHeapCap;
     int currHeapCap;
+    input *node;
 public:
     maxBinaryHeap(int arb);
-    void recursiveMax(int l1);
-    void addVal(input val);
     input pullTop();
     void deleteAll();
+    void recursiveMax(int l1);
+    void addVal(input val);
+
 
 
 };
@@ -30,27 +31,7 @@ maxBinaryHeap::maxBinaryHeap(int arb){
     node = new input[arb];
 }
 
-void maxBinaryHeap::recursiveMax(int l1){
-    if((l1 <= currHeapCap) && (currHeapCap / 2) < l1){
-        return;
-    }
-    int lchild = 1 + (2 * l1);
-    int rchild = 2 + (2 * l1);
-    if(node[l1].deaths < node[lchild].deaths || node[l1].deaths < node[rchild].deaths){
-        if(node[lchild].deaths <= node[rchild].deaths){
-            input arb;
-            arb = node[l1];
-            node[l1] = node[rchild];
-            node[rchild] = arb;
-        }else{
-            input arb;
-            arb = node[l1];
-            node[l1] = node[lchild];
-            node[lchild] = arb;
-        }
 
-    }
-}
 
 //search by extracting value
 void maxBinaryHeap::addVal(input val){
@@ -80,6 +61,30 @@ input maxBinaryHeap::pullTop(){
     return top;
 
 
+}
+
+void maxBinaryHeap::recursiveMax(int l1){
+    if((l1 <= currHeapCap) && (currHeapCap / 2) < l1){
+        return;
+    }
+    int lchild = 1 + (2 * l1);
+    int rchild = 2 + (2 * l1);
+    if(node[l1].deaths < node[rchild].deaths || node[l1].deaths < node[lchild].deaths){
+        if(node[lchild].deaths <= node[rchild].deaths){
+            input arb;
+            arb = node[l1];
+            node[l1] = node[rchild];
+            node[rchild] = arb;
+            recursiveMax(rchild);
+        }else{
+            input arb;
+            arb = node[l1];
+            node[l1] = node[lchild];
+            node[lchild] = arb;
+            recursiveMax(lchild);
+        }
+
+    }
 }
 
 
